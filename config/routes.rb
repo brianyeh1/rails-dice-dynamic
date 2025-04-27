4490200f030036f9fb40083056178ad45a88295e
brianyeh1 <nyeh@chicagobooth.edu>
2025-04-27 23:49:15 +0000

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-
+  get("", { :controller=> "dice", :action => "homepage"})
+  get("dice/:number_of_dice/:how_many_sides", { :controller=> "dice", :action => "dynamic"})
 end
